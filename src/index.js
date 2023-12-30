cc033c797c17ba2b98652971428202fe2cfe4097
Sebikostudio <sebikostudio@gmail.com>
2023-12-30 17:04:43 +0800

    Add components

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// index.js
+export { default as Avatar } from './Avatar';
+export { default as Badge } from './Badge';
+export { default as Button } from './Button';
+export { default as CheckboxCustom } from './CheckboxCustom';
+export { default as CodeHighlight } from './CodeHighlight';
+export { default as Flex } from './Flex';
+export { default as Heading } from './Heading';
+export { default as IconButton } from './IconButton';
+export { default as Iframe } from './Iframe';
+export { default as Input } from './Input';
+export { default as Paragraph } from './Paragraph';
+export { default as Separator } from './Separator';
+export { default as Tag } from './Tag';
+export { default as UserListItem } from './UserListItem';
+export { default as CustomToast } from './CustomToast';
+export { default as Youtube } from './Youtube';
+export { default as Styles } from './Variables.scss';
