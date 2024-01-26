4b43277c81f8520972582590572e97576f4d061a
Sebikostudio <sebikostudio@gmail.com>
2024-01-26 13:25:01 +0800

    fix path to loader

--- src/Spinner.js
+++ src/Spinner.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Lottie from "lottie-react";
-import loaderAnimation from "../assets/loader.json";
+import loaderAnimation from "./assets/loader.json";
 import "./Spinner.scss";
 import Flex from "./Flex";
 
